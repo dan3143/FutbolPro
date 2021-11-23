3085e31160f759e5e4433b8140419575cd817d5e
Víctor Daniel Guevara <vdguevara.31@gmail.com>
2021-11-23 14:16:36 -0500

    feat: add hook to get and filter countries

---
+++ apps/next/hooks/useCountries.ts
@@ -0,0 +1,38 @@
+import { Country } from '@futbol-pro/types';
+import { useEffect, useState } from 'react';
+import { getCountries } from '@futbol-pro/data-access-football-api';
+
+const useCountries = (): [
+  Array<Country>,
+  (search: string) => void,
+  boolean
+] => {
+  const [countries, setCountries] = useState<Array<Country>>([]);
+  const [filteredCountries, setFilteredCountries] = useState<Array<Country>>(
+    []
+  );
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getCountries().then((response) => {
+      setCountries(response);
+      setFilteredCountries(response);
+      setIsLoading(false);
+    });
+    return () => {
+      setCountries([]);
+      setIsLoading(true);
+    };
+  }, []);
+
+  const filterCountries = (search: string) => {
+    const filtered = countries.filter((country) =>
+      country.name.startsWith(search)
+    );
+    setFilteredCountries(filtered);
+  };
+
+  return [filteredCountries, filterCountries, isLoading];
+};
+
+export default useCountries;
