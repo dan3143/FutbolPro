be2fe7f87350e1c3a182f20d4c5029c07aee0eb4
Víctor Daniel Guevara <vdguevara.31@gmail.com>
2021-11-24 13:34:50 -0500

    build(tailwind): deactivate jit mode

--- apps/next/tailwind.config.js
+++ apps/next/tailwind.config.js
@@ -1,7 +1,6 @@
 const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
 
 module.exports = {
-  mode: 'jit',
   purge: createGlobPatternsForDependencies(__dirname),
   darkMode: false, // or 'media' or 'class'
   theme: {
