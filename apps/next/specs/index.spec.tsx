ad7d752b1730c363852fbb7467ef57343db9d62f
Víctor Daniel Guevara <vdguevara.31@gmail.com>
2021-11-24 14:41:52 -0500

    test: add tests for Countries component

--- apps/next/specs/index.spec.tsx
+++ apps/next/specs/index.spec.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Countries from '../pages/countries';
 
-import Index from '../pages/index';
+describe('Countries', () => {
+  it('should render countries', async () => {
+    render(<Countries />);
+    const colombia = await screen.findByRole('heading', { name: /colombia/i });
+    expect(colombia).toBeInTheDocument();
+  });
 
-describe('Index', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Index />);
-    expect(baseElement).toBeTruthy();
+  it('should filter countries', async () => {
+    render(<Countries />);
+    const searchInput = await screen.getByRole('textbox');
+    const brazil = await screen.findByRole('heading', { name: /brazil/i });
+    const colombia = await screen.findByRole('heading', { name: /colombia/i });
+    userEvent.type(searchInput, 'braz');
+    expect(brazil).toBeInTheDocument();
+    expect(colombia).not.toBeInTheDocument();
   });
 });
